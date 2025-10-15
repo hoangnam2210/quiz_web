53c6045728a05279ee0d8e456f7df2ddc85c1aba
NamNH7 <namnh7@mbbank.com.vn>
2025-10-15 09:15:14 +0700

    support dark theme

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"82bd5b7209295a5b7ff8cae0df96e7870171e3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3043585423"
+    serviceWorkerVersion: "2469307195"
   }
 });
